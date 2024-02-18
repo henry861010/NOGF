c7c6fcce1b366c45cf403637ba7ec1a1008c1bff
henry861010 <henry0978592955@gmail.com>
2024-02-18 21:55:55 +0800

    v0.0

--- uint/gps/gps.ino
+++ uint/gps/gps.ino
@@ -1,18 +1,13 @@
 #include <SoftwareSerial.h>
+#include <string.h>
 
 const int GPS_RX = 4;
 const int GPS_TX = 3;
-const int HC12_RX = 7;
-const int HC12_tX = 6;
 
-SoftwareSerial gps(GPS_RX, GPS_TX); 
-SoftwareSerial hc12(HC12_RX, HC12_tX); 
+SoftwareSerial gps(GPS_RX, GPS_TX);
 
 static char gpsmessage[100];
 static char gpsNow[100];
-static int length = 0;
-bool ifCopy = false;
-char c;
 
 bool ifGPRMC(char* s){
   if(s[1]=='G' and s[2]=='P' and s[3]=='R' and s[4]=='M' and s[5]=='C') return true;
@@ -20,15 +15,16 @@ bool ifGPRMC(char* s){
 }
 
 bool getMessage(){
-  if(gps.available()){
-    c = gps.read();
-    if(c=='\n'){
+  static int length = 0;
+  while(gps.available()){
+    char c = gps.read();
+    if(c=='\n'){ //get the eniter sentence from the gps
       gpsmessage[length]='\0';
       strcpy(gpsNow,gpsmessage);
       length = 0;
       return true;
     }
-    else{
+    else{ //get the char, but the sentence is not finish
       gpsmessage[length]=c;
       length++;
     }
@@ -44,10 +40,9 @@ void setup()
 
 void loop() // run over and over
 {
-  while(getMessage()){
+  if(getMessage()){
     if(ifGPRMC(gpsNow)){
       Serial.println(gpsNow);
-      hc12.write(gpsNow);
     }
   }
 }
